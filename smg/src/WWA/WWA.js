009adcc36f53bf377568f495d8503c34ff4eadf9
Torreythms <torreythms@gmail.com>
2020-06-02 16:54:21 -0400

    I am currently figuring out the image dimensions and have found one i would like to save

---
+++ smg/src/WWA/WWA.js
@@ -0,0 +1,48 @@
+import React from 'react';
+import "./WWA.css"
+
+
+const WWA = () => {
+
+return(
+ <div>
+
+
+   <div className="flex-row">
+     <div className="flex-image-1">
+
+     </div>
+     <div className="WWA-person-description">
+<h1> This is a header </h1>
+<p> This is a description </p>
+     </div>
+   </div>
+   <div className="flex-row">
+     <div className="flex-image-1">
+
+     </div>
+     <div className="WWA-person-description">
+<h1> This is a header </h1>
+<p> This is a description </p>
+     </div>
+   </div>
+
+   <div className="flex-row">
+     <div className="flex-image-1">
+
+     </div>
+     <div className="WWA-person-description">
+<h1> This is a header </h1>
+<p> This is a description </p>
+     </div>
+   </div>
+
+
+
+
+</div>
+)
+
+}
+
+export default WWA;
