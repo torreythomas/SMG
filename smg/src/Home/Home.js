7157b81089990ab55732c5d89e508ff683086a0d
Torreythms <torreythms@gmail.com>
2020-06-02 14:02:32 -0400

    first push update

--- smg/src/Home/Home.js
+++ smg/src/Home/Home.js
@@ -6,21 +6,21 @@ const Home = () => {
 
 return(
     <div>
-        <div className="home-navigation-display-container">
-   <Navbar />
-   </div>
    <section className="home-main-section-container">
    <div className="home-flex-container">
         <div className="home-column item1"> 
-            <h1 className="home-smg-title"> S.EE</h1>
-            <h1 className="home-smg-title more"> M.ORE</h1>
-            <h1 className="home-smg-title"> G.OOD</h1>
+        <span role="img"  description="aria-label" className="smg-home"> 🌐  </span>
+
+            <h1 className=" home-smg-title see  animate__animated"> <b className="S">S</b>.EE</h1>
+            <h1 className="home-smg-title more"> <b className="S">M</b>.ORE</h1>
+            <h1 className="home-smg-title good"> <b className="S">G</b>.OOD</h1>
             <p className="home-smg-description">
         An initiative designed to spread more positive images to the youth of Central Pennsylvania through the game of basketball and financial literacy knowledge. </p>
-            <a className=" grow home-contact-button" href="mailto:destinedfordollars@gmail.com">Contact Us</a> 
+            <a className=" grow home-contact-button" href="mailto:destinedfordollars@gmail.com">Who Are We?</a> 
         </div>
-  <div className="home-column item2 shadow5"></div>
-  <div className="home-column item3 shadow5"></div>
+  <div className="home-column item2 grow dib shadow5"></div>
+  <div className="home-column item3 grow dib shadow5"></div>
+  <div className="home-column item4 grow dib shadow5"></div>
 </div>
 </section>
 </div>
