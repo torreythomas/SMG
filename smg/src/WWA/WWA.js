043f81e7dc5f341cf21b68ab7a73b48d6a36547d
Torreythms <torreythms@gmail.com>
2020-06-03 12:31:00 -0400

    first push daily update

--- smg/src/WWA/WWA.js
+++ smg/src/WWA/WWA.js
@@ -6,38 +6,39 @@ const WWA = () => {
 
 return(
  <div>
-
-
-   <div className="flex-row">
-     <div className="flex-image-1">
-
-     </div>
-     <div className="WWA-person-description">
-<h1> This is a header </h1>
-<p> This is a description </p>
-     </div>
-   </div>
-   <div className="flex-row">
-     <div className="flex-image-1">
-
-     </div>
-     <div className="WWA-person-description">
-<h1> This is a header </h1>
-<p> This is a description </p>
-     </div>
-   </div>
+<div id="WWA-landing-section">
+  Hey
+</div>
 
    <div className="flex-row">
      <div className="flex-image-1">
 
      </div>
      <div className="WWA-person-description">
-<h1> This is a header </h1>
-<p> This is a description </p>
+<h1 className="wwa-personal-header"> Jared Young </h1>
+<p className="wwa-personal-description">  a rising Senior at Princeton majoring in Politics, with a minor in African American studies. Jared graduated from Episcopal High School in VA, where he won the varsity basketball coaches award, was an honor roll student every semester, and served as a Senior Monitor, among many other extracurriculars. In college he has continued to pursue his passion for basketball by being a youth basketball coach in the community, serving as a practice player for Princeton’s nationally-ranked women’s basketball team, and by serving as the captain of Princeton Club Basketball.  He has pursued his passion for business by maintaining a 3.7 GPA, while working internships at Morgan Stanley, Martha’s Table, and McKinsey, while also being a Management Leadership for Tomorrow fellow, among other things. He one day hopes to be a CEO of a sports team or the Commissioner of the NBA.  </p>
+     </div>
+   </div>
+   
+   <div className="flex-row2">
+     <div className="flex-image-2">
+     </div>
+     <div className="WWA-person-description">
+<h1 className="wwa-personal-header"> Jaime Orr </h1>
+<p className="wwa-personal-description">  a rising Junior at Central Penn College, majoring in Business. Jaime graduated from York Catholic High School, where he played Varsity Basketball and was an honor roll student. Mr. Orr has continued his athletic career in college, playing point guard for Central Penn College’s Men’s Basketball team. He is passionate about his community and plans to be a community leader and organizer in the future.  </p>
      </div>
    </div>
 
-
+ 
+   <div className="flex-row">
+     <div className="flex-image-3">
+     </div>
+     <div className="WWA-person-description">
+<h1 className="wwa-personal-header"> Melik Martin </h1>
+<p className="wwa-personal-description"> a rising Senior at Monmouth University, majoring in Business Marketing. Melik graduated from York Catholic High School, where he played Varsity Basketball and was an all-county and all-district performer. Melik chose to continue his athletic career with the Monmouth Hawks, after mulling over a myriad of scholarship offers. Melik is extremely passionate about community service, marketing, and basketball. After a professional basketball career, Melik wants to run his own business and be a philanthropist.   </p>
+     </div>
+   </div>
+    
 
 
 </div>
