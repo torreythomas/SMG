ec14eb5a8b43bb0673066a74e7d3630dbd86c50f
Torreythms <torreythms@gmail.com>
2020-06-05 21:02:47 -0400

    finished for the day

--- smg/src/Misson/Mission.js
+++ smg/src/Misson/Mission.js
@@ -4,7 +4,7 @@ import { Link } from "react-router-dom";
 import video from '../images/JaredFacts.mp4'
 import video2 from '../images/yoma.MOV'
 import video3 from '../images/WeBeHoopin.mp4'
-import image from "../images/SMG\ Day\ Flyer.jpg.jpeg"
+import Scroll from '../Scroll'
 
 
 const Mission = () => {
@@ -15,55 +15,43 @@ const Mission = () => {
 < Link className="  link-mission animate__animated animate_Bounce" to="/WWA"> 
           <p className=" grow shadow-5 link-mission home-contact-button"> We Are </p> 
 </Link> 
-< Link className="   animate__animated animate_Bounce" to="/WWA"> 
+< Link className="animate__animated animate_Bounce" to="/WWA"> 
           <p className=" grow shadow-5  home-contact-button"> Contact </p> 
 </Link> 
 </div>
 
-<h1 id="who-are-we-header-main-text" style={{color:"black"}}>
-    Our Mission
+<h1 id="who-are-we-header-main-text" style={{color:"white", marginTop:"-180px"}}>
+    Our Mission 
   </h1>
 </div>
+
+
         <div id="mission-container">
-        <h1 className="we-are-on-a-mission"> We Are On A Mission </h1>
-        <p className="a-meassage-from-jared"> <em>A message from Jared Young of the SMG family on the education system.</em></p>
        <div className="mission-statement-container">
         <div id="mission-landing-page-video-section">
+            <div className="mission-landing-page-video-section-description">
+               <h1> We believe change begins with <em style={{color:"goldenrod"}}>education. </em> </h1>
+               <p className="mission-landing-page-video-section-description-text"> There is no limit to the potential of a kid given a chance. We seek to use basketball and education
+                   to influence a new standard in our community. 
+               </p>
+            </div>
         <video className="mission-video" className="shadow-5 dib" width="720" height="540"  controls>
   <source src={video} type="video/mp4"/>
   <source src={video}  type="video/mp4"/>
 Your browser does not support the video tag.
 </video>
          </div>
-         <div id="mission-landing-page-video-section">
-        <div className="mission-image3-video-section grow">
-                    
-                    </div>
          </div>
-         </div>
-            <div className="photo-container  ba bw4 shadow-5">
-                <div className=" mission-image1 grow">
-
-                </div>
-                <div className="mission-image2 grow">
-                    
-                    </div>
-                    <div className="mission-image3 grow">
-                    
-                    </div>
-                    <div className="mission-image4 grow">
-                    
-                    </div>
-            </div>
-<h1 className="see-more-good-in-our-community"> We Desire to See More <em> GOOD </em> in our community</h1>
-<div className="our-mission-container grow shadow-5">
-        <h1 className="mission-statement-header"><u> SMG </u> </h1>
+<div className="huh">
+         <div className="our-mission-container grow shadow-5">
+        <h1 className="mission-statement-header"><u> See More Good </u> </h1>
         <p className="mission-statement-text">
         SMG is an initiative designed to spread more positive images to the youth of Central Pennsylvania through the game of basketball. SMG also aims to empower the next generation with financial literacy knowledge. SMG is a lifestyle, a commitment to learn, improve, and dominate in every aspect of life. 
         
         </p>
         </div>
-        <div className="our-goals-container grow shadow-5">
+        <div className="goals-and-mission-container">
+         <div className="our-goals-container grow shadow-5">
         <h1 className="our-goals-header"><u> Goals</u> </h1>
         <p className="our-goals-text">
         We will use the game of basketball to provide an outlet to our youth so that they can escape violence, negativity, financial illiteracy, and division. We will hold events that bring our community light, happiness, and fun. We will allow everyone who comes in contact with our organization to see the good in life. 
@@ -73,34 +61,22 @@ Your browser does not support the video tag.
         <h1 className="mission-statement-header"><u> Mission</u> </h1>
         <p className="mission-statement-text">
         The cup of life can either be half-full or half-empty, it’s all about perspective. We at SMG choose to see the good in life, and our mission is to spread optimism and hope to the youth of central Pennsylvania through the game of basketball. 
-        
         </p>
         </div>
-
-
         </div>
+        </div>
+         
+         <div id="upcoming-events-section">
+<h1 style={{textAlign:"center"}}> <strong>Upcoming Events </strong></h1>             
+<div className="mission-image1 grow">
+    </div>
 
-        <h1 className="we-are-on-a-mission"> Using Basketball to transform and shift the narrative </h1>
-
-        <div id="mission-landing-page-video-section">
-        <video className="mission-video" 
-        style={{ backgroundColor: "rgba(0,0,0,0.5"}} ßclassName="shadow-5 dib" width="720" height="540"  controls>
-  <source src={video2} type="video/mp4"/>
-  <source src={video2}  type="video/mp4"/>
-Your browser does not support the video tag.
-</video>
+   
+  
          </div>
 
 
-         <h1 className="we-are-on-a-mission"> Upcoming Events </h1>
-
-<p className="click-to-register"> Click to register </p>
-<a href="https://l.instagram.com/?u=https%3A%2F%2Fdocs.google.com%2Fforms%2Fd%2Fe%2F1FAIpQLSevvWnoGsAQdbIRw_5L8AhkE2bK7pI4bAFIwWvvkqTY-XEc-g%2Fviewform&e=ATOWiftUFe3CWbTh5_6Lh1fd2TzElYu6m5pilMwL0S07e3lDYpBAvYSkewHDMTCgSiGfPWjzrRC6kguS&s=1" className="a-mission-link">
-<img src={image} className="images" ></img>
-</a>
- 
-
-
+        </div>
         </div>
     )
 }
---
+++ smg/src/Scroll.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+const Scroll = (props) => {
+    return (
+        <div style={{             
+            overflowY: 'scroll',
+            height: '80vh',
+    
+        }}>
+
+
+        {props.children}
+        </div>
+    )
+    
+}
+
+export default Scroll; 
